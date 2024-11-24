696838534181464ef1ea56b7ca12eb7bb9a0e91b
Aarya Chopkar <aaryachopkar@gmail.com>
2024-11-24 22:35:56 +0530

    fix: image url

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "avatars.githubusercontent.com",
+        port: "", // Leave empty for default port
+        pathname: "/u/**", // Match any user avatar
+      },
+    ],
   },
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
