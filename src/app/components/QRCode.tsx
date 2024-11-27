8e62d7b41a8b95b73cc80f98e7cdc0a47e78ccdb
Aarya Chopkar <aaryachopkar@gmail.com>
2024-11-27 14:35:51 +0530

    feat: added qrcode

---
+++ src/app/components/QRCode.tsx
@@ -0,0 +1,24 @@
+import QRCode from "react-qr-code";
+import React from "react";
+
+interface QRCodeDivProps {
+  value: string;
+}
+
+const QRCodeDiv: React.FC<QRCodeDivProps> = ({ value }) => {
+  return (
+    <div
+      className="border border-[#F7D635]"
+      style={{ height: "auto", margin: "0 auto", maxWidth: 144, width: "100%" }}
+    >
+      <QRCode
+        size={256}
+        style={{ height: "auto", maxWidth: "100%", width: "100%" }}
+        value={value}
+        viewBox="0 0 256 256"
+      />
+    </div>
+  );
+};
+
+export default QRCodeDiv;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,16 +15,37 @@ export default {
         "dark-radial":
           "radial-gradient(circle at 50% 50%, #333 20%, transparent 21%, transparent 49%, #333 50%)",
       },
+
       animation: {
         blur: "blurAnimate 5s ease-in-out infinite alternate",
       },
+
       keyframes: {
         blurAnimate: {
           "0%": { backdropFilter: "blur(1px)" },
           "100%": { backdropFilter: "blur(5px)" },
         },
       },
+
+      transform: {
+        "preserve-3d": "preserve-3d",
+      },
+      rotate: {
+        "y-180": "rotateY(180deg)",
+      },
     },
   },
-  plugins: [],
+
+  plugins: [
+    function ({ addUtilities }: any) {
+      addUtilities({
+        ".backface-hidden": {
+          backfaceVisibility: "hidden",
+        },
+        ".backface-visible": {
+          backfaceVisibility: "visible",
+        },
+      });
+    },
+  ],
 } satisfies Config;
